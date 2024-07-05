52081d4230272cdc64823c1791702864395da097
Erica Marigold <hi@devcomp.xyz>
2024-07-05 23:56:55 +0530

    Remove unneeded locales

--- src/i18n/languages/en.ts
+++ src/i18n/languages/en.ts
@@ -4,7 +4,7 @@ import type { Translation } from "../translation";
 export const en: Translation = {
   [Key.home]: "Home",
   [Key.about]: "About",
-  [Key.archive]: "Archive",
+  [Key.archive]: "Timeline",
   [Key.search]: "Search",
 
   [Key.tags]: "Tags",
--- src/i18n/translation.ts
+++ src/i18n/translation.ts
@@ -1,9 +1,6 @@
 import { siteConfig } from "../config";
 import type I18nKey from "./i18nKey";
 import { en } from "./languages/en";
-import { ja } from "./languages/ja";
-import { zh_CN } from "./languages/zh_CN";
-import { zh_TW } from "./languages/zh_TW";
 
 export type Translation = {
   [K in I18nKey]: string;
@@ -16,10 +13,6 @@ const map: { [key: string]: Translation } = {
   en_us: en,
   en_gb: en,
   en_au: en,
-  zh_cn: zh_CN,
-  zh_tw: zh_TW,
-  ja: ja,
-  ja_jp: ja,
 };
 
 export function getTranslation(lang: string): Translation {
