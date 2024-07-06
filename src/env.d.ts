675338a7bbd554dfce9a1b8294937f4b2b47f7a8
Erica Marigold <hi@devcomp.xyz>
2024-07-06 21:57:01 +0530

    Add git commit hash with link to footer component

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly COMMIT_HASH?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
